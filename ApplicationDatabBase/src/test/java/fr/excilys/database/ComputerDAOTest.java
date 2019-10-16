d797dfc27294cfd98778f60098fa99fe6f12cde2
excilys <excilys@Laptop0007>
2019-10-16 15:27:11 +0200

    Feature : Ajout de la connection à la base de données H2

---
+++ ApplicationDatabBase/src/test/java/fr/excilys/database/ComputerDAOTest.java
@@ -0,0 +1,14 @@
+package fr.excilys.database;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComputerDAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
