32e84d1ae88d5b7859eb084b0230f9dc0e35447f
excilys <excilys@Laptop0007>
2019-11-04 14:34:47 +0100

    tests with annotation Spring

--- ApplicationDatabBase/src/test/java/fr/excilys/database/CompanyDAOTest.java
+++ ApplicationDatabBase/src/test/java/fr/excilys/database/CompanyDAOTest.java
@@ -6,22 +6,31 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import fr.excilys.database.configuration.ApplicationConfig;
 import fr.excilys.database.dao.imp.CompanyDAOImpl;
 import fr.excilys.database.model.Company;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ApplicationConfig.class})
 public class CompanyDAOTest {
-	CompanyDAOImpl company;
+	
+	@Autowired
+	CompanyDAOImpl companyDAO;
 	
 	@Before
 	public void makeH2Connection() {
-		System.setProperty("testing","true");
-		company=new CompanyDAOImpl();			
+		System.setProperty("testing","true");		
 	}
 	
 	@Test
 	public void testgetAllCompanies() {
-		List<Company> mycompanies=company.getAllCompanies();
+		List<Company> mycompanies=companyDAO.getAllCompanies();
 		assertEquals(mycompanies.size(),7);
 	}
 }
