435ae4ae6c453f6e2a30a2b4260289025c8549e6
excilys <excilys@Laptop0007>
2019-11-19 17:18:49 +0100

    Project Multi-module

---
+++ src/main/java/fr/excilys/database/App.java
@@ -0,0 +1,13 @@
+package fr.excilys.database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
